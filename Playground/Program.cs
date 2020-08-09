2f49415074a1ab3e52aab59bc8b92de3f22b0836
Tobias Freese <wolfshund98@gmail.com>
2020-08-09 19:06:13 +0200

    Create playground

---
+++ Playground/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using static System.Console;
+
+namespace Playground
+{
+    class Program
+    {
+        private static (int, int) _windowSize = (0, 0);
+
+        static void Main(string[] args)
+        {
+            double progress = 0;
+            while (true)
+            {
+                ShowProgress(progress);
+                progress += 1;
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void ShowProgress(double percent)
+        {
+            if (WindowWidth < 22 && WindowHeight < 5) return;
+            if (_windowSize != (WindowWidth, WindowHeight))
+            {
+                _windowSize = (WindowWidth, WindowHeight);
+                Clear();
+                Write("Transferring...");
+            }
+
+            try
+            {
+                SetCursorPosition(16, 0);
+                Write($"{percent}%");
+
+                SetCursorPosition(0, WindowHeight - 2);
+                var barLength = Math.Ceiling(WindowWidth * (percent / 100));
+                var builder = new StringBuilder();
+                for (var i = 0; i < barLength; i++)
+                {
+                    builder.Append("=");
+                }
+
+                Write(builder.ToString().PadRight(WindowWidth));
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Clear();
+            }
+        }
+    }
+}
